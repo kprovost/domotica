5abec96365323087c5aeec2642b6598c169c9a27
Kristof Provost <kristof@codepro.be>
2013-07-20 16:37:13 +0200

    Read light status from the PLC

--- domotica/light.py
+++ domotica/light.py
@@ -1,9 +1,19 @@
 import lightconf
+import s7
 
 class Light:
-    def __init__(self, name, id):
+    def __init__(self, name, id, s7conn):
         self._name = name
         self._id = id
+        self._s7conn = s7conn
+
+        self.STATUS_DB = 12
+        self.TIMEOUT_DB = 15
+
+        self.TOGGLE_BIT = 5          # write only
+        self.STATUS_BIT = 6          # read only
+        self.MOTION_TRIGGER_BIT = 7  # read/write
+        self.BLINK_ON_MOTION_BIT = 8 # read/write
 
     def getName(self):
         return self._name
@@ -12,7 +22,7 @@ class Light:
         return self._id
 
     def isOn(self):
-        return (self._id / 2) % 2 == 0
+        return self._s7conn.readBit(self.STATUS_DB, self._id, self.STATUS_BIT)
 
     def getTimeout(self):
         return 10
@@ -20,10 +30,10 @@ class Light:
     def toggle(self):
         return False
 
-def loadAll():
+def loadAll(s7conn):
     lights = [ ]
     for name, id in lightconf.lights:
-        l = Light(name, id)
+        l = Light(name, id, s7conn)
         lights.append(l)
     return lights
 
--- domotica/views.py
+++ domotica/views.py
@@ -1,17 +1,21 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+import s7
 
 import light
 
 def index(request):
-    lights = light.loadAll()
+    s7conn = s7.S7Comm("10.0.3.9")
+
+    lights = light.loadAll(s7conn)
     context = { 'lights' : lights }
     return render(request, "lights.html", context)
 
 @csrf_exempt
 def lightswitch(request, action):
-    l = light.Light("", request.REQUEST["id"])
+    s7conn = s7.S7Comm("10.0.3.9")
+    l = light.Light("", request.REQUEST["id"], s7conn)
 
     if action != "toggle":
         raise Http404
