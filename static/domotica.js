4941fb0739d7607243b014f0ced02f89ac3f664c
Kristof Provost <kristof@codepro.be>
2013-07-20 22:34:42 +0200

    Handlers for the motion detector and blink settings.
    Note that the django backend isn't implemented yet, but the python bits
    are simple.

--- static/domotica.js
+++ static/domotica.js
@@ -1,16 +1,54 @@
-function lightswitch()
+function toggle_light()
 {
     id = this.id.replace("light_", "");
     $.post("/lightswitch/toggle", { id: id })
         .fail(function(){
-                console.log("Failed to post lightswitch/toggle" + id);
+                console.log("Failed to post lightswitch/toggle " + id);
                 location.reload();
             });
 };
 
-$(document).ready(function () {
-    $(".toggle").each(function(index) {
-        obj = $(".toggle")[index];
-        document.querySelector('#' + obj.id).addEventListener('toggle', lightswitch, obj.id);
+function toggle_motion()
+{
+    id = this.id.replace("motion_", "");
+    $.post("/lightswitch/toggle_motion", { id: id })
+        .fail(function(){
+                console.log("Failed to post lightswitch/toggle_motion " + id);
+                location.reload();
+            });
+};
+
+function toggle_blink()
+{
+    id = this.id.replace("blink_", "");
+    $.post("/lightswitch/toggle_blink", { id: id })
+        .fail(function(){
+                console.log("Failed to post lightswitch/toggle_blink " + id);
+                location.reload();
+            });
+};
+
+function installPostHandlers() {
+    $(".light").each(function(index) {
+        obj = $(".light")[index];
+        document.querySelector('#' + obj.id).addEventListener('toggle', toggle_light);
     });
+
+    $(".motion").each(function(index) {
+        obj = $(".motion")[index];
+        document.querySelector("#" + obj.id).addEventListener('toggle', toggle_motion);
+    });
+
+    $(".blink").each(function(index) {
+        obj = $(".blink")[index];
+        document.querySelector("#" + obj.id).addEventListener('toggle', toggle_blink);
+    });
+};
+
+$(document).ready(function () {
+    installPostHandlers();
+});
+
+window.addEventListener('push', function() {
+    installPostHandlers();
 });
