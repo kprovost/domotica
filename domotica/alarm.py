1fab87d81cb33611428b4edf5681439aa131254d
Kristof Provost <kristof@codepro.be>
2014-04-21 21:46:11 +0200

    Read alarm status
    We can now retrieve the the arming status of the alarm, as well as the
    alarm-triggered state.

--- domotica/alarm.py
+++ domotica/alarm.py
@@ -17,7 +17,7 @@ class Alarm:
         self._s7conn.writeBit(self.ALARM_DB, 0, 2, 0)
 
     def isArmed(self):
-        return False
+        return self._s7conn.readFlagBit(5, 2)
 
     def isAlarmTriggered(self):
-        return False
+        return self._s7conn.readFlagBit(5, 3)
