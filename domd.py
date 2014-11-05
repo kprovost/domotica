6aaf6f574560dc47f2ec665a6742edecd1625b33
Kristof Provost <kristof@codepro.be>
2014-11-05 14:51:49 +0100

    Make daemonizing optional
    Introduce command line argument parsing, add a single --foreground
    option so we can keep the daemon on the foreground for development.

--- domd.py
+++ domd.py
@@ -7,6 +7,8 @@
 ###
 
 import daemon
+import optparse
+
 import domotica.alarm
 import domotica.settings as settings
 import domotica.alarm as alarm
@@ -21,8 +23,14 @@ def test():
         print("Alarm triggered too!")
 
 def main():
-    d = daemon.DaemonContext(prevent_core=False)
-    d.open()
+    parser = optparse.OptionParser()
+    parser.add_option("-f", "--foreground", action="store_true",
+            dest="foreground", help="Do not daemonize", default=False)
+    (options, args) = parser.parse_args()
+
+    if not options.foreground:
+        d = daemon.DaemonContext(prevent_core=False)
+        d.open()
 
     test()
 
