697001ebc1bc19ef11272a53d13efc79a3910d00
Kristof Provost <kristof@codepro.be>
2014-11-15 00:53:57 +0100

    Temperature logger cleanups
    Preparing the class to be usable by both domd.py (to log the
    temperature) and the web app (to render and show the graphs).

--- pollers/temperaturepoller.py
+++ pollers/temperaturepoller.py
@@ -1,33 +1,51 @@
 import rrdtool
 import logging
+import os
 import domotica.heating as heating
 from poller import Poller
 import s7
 
 class TemperaturePoller(Poller):
-    def __init__(self):
-        return
-        rrdtool.create( 'temperature.rrd',
-                '--no-overwrite',
-                '--step', '5',
-                'DS:temperature:GAUGE:30:-20:100',
-                'RRA:AVERAGE:0.5:1:1440',   # Daily
-                'RRA:AVERAGE:0.5:12:10080', # Weekly,
-                'RRA:AVERAGE:0.5:720:365')  # Yearly
+    RRD_FILE = "stable_temperature.rrd"
+    PNG_FILE = "/tmp/stable_temperature.png"
+
+    def __init__(self): # TODO Add poll interval parameter
+        if not os.path.exists(self.RRD_FILE):
+            self.create()
+
+    def create(self):
+        try:
+            rrdtool.create(self.RRD_FILE,
+                    '--no-overwrite',
+                    '--step', '5',
+                    'DS:temperature:GAUGE:30:-20:100',
+                    'RRA:AVERAGE:0.5:1:1440',   # Daily
+                    'RRA:AVERAGE:0.5:12:10080', # Weekly,
+                    'RRA:AVERAGE:0.5:720:365')  # Yearly
+        except Exception, e:
+            logging.error("Failed to create file: %s" % e)
 
     def poll(self, s7conn):
-        rrdtool.update('temperature.rrd',
-                'N:19.0')
+        h = heating.Heating(s7conn)
+        temp = h.getCurrent()
+        try:
+            rrdtool.update(self.RRD_FILE, "N:%.1f" % temp)
+        except Exception, e:
+            logging.error("Failed to log temperature: %s" % e)
 
-        rrdtool.graph('temperature.png',
-                '--imgformat', 'PNG',
-                '--width', '540',
-                '--height', '100',
-                #'--start', "-%i" % 2014,
-                #'--end', "-1",
-                '--vertical-label', 'Degrees Celcius',
-                '--title', 'Temperature',
-                #'--lower-limit', '0',
-                'DEF:temperature=temperature.rrd:temperature:AVERAGE',
-                'AREA:temperature#990033:Downloads')
-        pass
+    def draw(self, start):
+        try:
+            rrdtool.graph(self.PNG_FILE,
+                    '--imgformat', 'PNG',
+                    '--width', '540',
+                    '--height', '100',
+                    '--start', "-%s" % start,
+                    #'--end', "-1",
+                    '--vertical-label', 'Graden Celcius',
+                    '--title', 'Temperature',
+                    #'--lower-limit', '0',
+                    "DEF:temperature=%s:temperature:AVERAGE" % self.RRD_FILE,
+                    'AREA:temperature#990033:Temperatuur')
+        except Exception, e:
+            logging.error("Failed to draw temperature: %s" % e)
+            raise
